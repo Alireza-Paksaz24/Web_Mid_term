8b4a317ef8a1d96592625e69c4380b585a72bb0f
Alireza-Paksaz24 <a.paksaz@outlook.com>
2023-12-17 14:41:48 +0330

    add handlers function in main.go

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"log"
+
+	handler "github.com/Alireza-Paksaz24/Web_Mid_term"
 	"github.com/labstack/echo"
 )
 
-func main(){
-	
+func main() {
+	e := echo.New()
+
+	h := handler.Handler{}
+
+	e.GET("/basket", h.GET)
+
+	e.POST("/basket", h.CreateBasket)
+
+	e.PATCH("/basket", h.UpdateBasket)
+
+	e.GET("/basket", h.GetBasketByID)
+
+	e.DELETE("/basket", h.DELETE)
+
+	if err := e.Start(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
